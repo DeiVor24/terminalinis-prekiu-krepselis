a7cfb39cabdcda7bcf5fdbfe89155ea63b322387
DeiVis <zbitkisas@gmail.com>
2024-08-15 15:41:58 +0300

    emptylist

--- js/shoppingList.js
+++ js/shoppingList.js
@@ -1,38 +1,45 @@
-function shoppingList(emptyList) {
-    return ((emptyList + emptyList.length) === 0),'Šiuo metu, jūsų prekių krepšelis yra tuščias.'
-};
+// function shoppingList(emptyList) {
+//     return ((emptyList + emptyList.length) === 0),'Šiuo metu, jūsų prekių krepšelis yra tuščias.'
+// }
+// return emptyList.length > 0)
+// ;
+// const emptyList = [];
+// console.log(shoppingList(emptyList));
+
 const emptyList = [];
+function shoppingList(i) {
+    return (i === 0),'Šiuo metu, jūsų prekių krepšelis yra tuščias.';
+
+}
 console.log(shoppingList(emptyList));
 
 
+// const firstShoppingList = [
+//     {
+//         id: 1,
+//         name: 'Pomidoras',
+//         amount: 1000000,
+//         unitPrice: 199,
+//     },
+//     {
+//         id: 3,
+//         name: 'Agurkas',
+//         amount: 2,
+//         unitPrice: 50,
+//     },
+//     {
+//         id: 7,
+//         name: 'Svogūnas',
+//         amount: 1,
+//         unitPrice: 45,
+//     },
+// ];
 
 
-const firstShoppingList = [
-    {
-        id: 1,
-        name: 'Pomidoras',
-        amount: 1000000,
-        unitPrice: 199,
-    },
-    {
-        id: 3,
-        name: 'Agurkas',
-        amount: 2,
-        unitPrice: 50,
-    },
-    {
-        id: 7,
-        name: 'Svogūnas',
-        amount: 1,
-        unitPrice: 45,
-    },
-];
+// console.log(firstShoppingList.length);
 
 
-console.log(firstShoppingList.length);
-
-
-console.log(`Jūsų prekių krepšelyje yra ${firstShoppingList.length}  prekės:`);
+// console.log(`Jūsų prekių krepšelyje yra ${firstShoppingList.length}  prekės:`);
 
 
 
