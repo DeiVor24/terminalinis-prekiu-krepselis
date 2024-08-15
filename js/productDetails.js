2acdab1d812675cedf540a3ff559519e1bdb4da4
DeiVis <zbitkisas@gmail.com>
2024-08-15 16:12:10 +0300

    nzn

--- js/productDetails.js
+++ js/productDetails.js
@@ -5,6 +5,41 @@
 
 
 
+
+
+const emptyList = [];
+function productDetails([], i) {
+    return (i <= firstShoppingList.length) ? firstShoppingList[i] : `Prekė, su ID:  ${i}  neegzistuoja.`;
+}
+
+
+// function productDetails (){
+//     for (let i = 0; i < firstShoppingList.length; i++);
+// } 
+
+
+const firstShoppingList = [
+    {
+        id: 1,
+        name: 'Pomidoras',
+        amount: 1000000,
+        unitPrice: 199,
+    },
+    {
+        id: 3,
+        name: 'Agurkas',
+        amount: 2,
+        unitPrice: 50,
+    },
+    {
+        id: 7,
+        name: 'Svogūnas',
+        amount: 1,
+        unitPrice: 45,
+    },
+];
+
+console.log(productDetails(emptyList, ));
    
 
     
