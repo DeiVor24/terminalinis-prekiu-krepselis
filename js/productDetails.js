d9fbccddcdc1eb33b1fd2f3e2aeeeb9dde9be282
DeiVis <zbitkisas@gmail.com>
2024-08-29 08:21:25 +0300

    asd

--- js/productDetails.js
+++ js/productDetails.js
@@ -1,11 +1,3 @@
-export function visaKaina(preke) {
-    console.log("------------------------------");
-    console.log("Prekės informacija");
-    console.log("------------------------------");
-    console.log(`ID            |  ${preke.id}`);
-    console.log(`Pavadinimas   |  ${preke.pavadinimas}`);
-    console.log(`Kiekis        |  ${preke.kiekis} vnt`);
-    console.log(`Vieneto kaina |  ${preke.kaina.toFixed(2)} Eur`);
-    console.log(`Viso mokėti   |  ${visaKaina(preke)} Eur`);
-    console.log("------------------------------");
+export function productDetails (){
+    
 }
--- js/shoppingList.js
+++ js/shoppingList.js
@@ -1,35 +1,6 @@
-export function emptyList(list) {
-    return list === 0 ? 'Šiuo metu, jūsų prekių krepšelis yra tuščias.' : list;
-
-}
-
-export function prekesPatikra (preke){
-if (!preke.includes(preke)) {
-    return `Prekė, su ID: ${preke} neegzistuoja.`;
-}
-}
-
-export function visaKaina(preke) {
-    return (preke.kiekis * preke.kaina).toFixed(2);
-}
-
-
-export function krepselis (krepselis){
-    console.log(`Jūsų prekių krepšelyje yra ${prekes.length > 1 ? 'ės' : 'ė'}`);
-console.log("-----------------------------------------------------------");
-console.log("Pavadinimas  | Kiekis      | Vieneto kaina | Viso mokėti");
-console.log("-----------------------------------------------------------");
-
-krepselis.forEach((preke, index) => {
-    const visoMoketi = visaKaina(preke);
-    const pavadinimas = preke.pavadinimas.padEnd(preke.length);
-    const kiekis = (`${preke.kiekis} +  vnt`).padEnd(preke.length);
-    const kaina = (`${preke.kaina.toFixed(2)} + Eur`).padEnd(preke.length);
-    const viso = `${visoMoketi}  Eur`;
+export function shoppingList (){
     
-    console.log(`${index + 1}. ${pavadinimas} | ${kiekis} | ${kaina} | ${viso}`);
-});
-
-console.log("-----------------------------------------------------------");
 }
+
+
 
