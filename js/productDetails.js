cc73ef6808c011330acdd96e7b13f7f6b08dfa79
DeiVis <zbitkisas@gmail.com>
2024-09-15 20:02:19 +0300

    up

--- js/productDetails.js
+++ js/productDetails.js
@@ -1,3 +1,22 @@
-export function productDetails (){
+export function productDetails(emptyList, productId) {
+    const product = emptyList.find(item => item.id === productId);
+    if (!product) {
+        return `Prekė, su ID: ${productId} neegzistuoja.`;
+    }
     
+    let totalPrice = (product.amount * product.unitPrice / 100).toFixed(2);
+    let unitPrice = (product.unitPrice / 100).toFixed(2); 
+
+    let result = '------------------------------\n';
+    result += 'Prekės informacija\n';
+    result += '------------------------------\n';
+    result += `ID            | ${product.id}\n`;
+    result += `Pavadinimas   | ${product.name}\n`;
+    result += `Kiekis        | ${product.amount} vnt\n`;
+    result += `Vieneto kaina | ${unitPrice} Eur\n`;
+    result += `Viso mokėti   | ${totalPrice} Eur\n`;
+    result += '------------------------------';
+
+    return result;
+
 }
--- js/shoppingList.js
+++ js/shoppingList.js
@@ -1,6 +1,24 @@
-export function shoppingList (){
+export function shoppingList(emptyList) {
+    if (emptyList.length === 0) {
+        return 'Šiuo metu, jūsų prekių krepšelis yra tuščias.';
+    }
     
-}
+let totalItems = emptyList.length;
+let result = `Jūsų prekių krepšelyje yra ${totalItems} prekės:\n`;
+result += '-----------------------------------------------------------\n';
+result += 'Pavadinimas  | Kiekis      | Vieneto kaina | Viso mokėti\n';
+result += '-----------------------------------------------------------\n';
 
+emptyList.forEach((item, index) => {
+    let totalPrice = (item.amount * item.unitPrice / 100).toFixed(2); 
+    let unitPrice = (item.unitPrice / 100).toFixed(2); 
+
+    result += `${index + 1}. ${item.name.padEnd(10)} | ${item.amount} vnt       | ${unitPrice} Eur      | ${totalPrice} Eur\n`;
+});
+
+result += '-----------------------------------------------------------\n';
+
+return result;
+}
 
 
